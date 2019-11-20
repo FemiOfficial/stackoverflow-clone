4bf9dcb76028bba3310b0f13bc0978191684869c
Alayesanmi Femi <phemzyoflife@gmail.com>
2019-11-20 23:04:28 +0100

    Revert "Merge Into Master from Develop Branch"

--- src/middlewares/trimInputs.js
+++
@@ -1,9 +0,0 @@
-const trimInputs = (request, response, next) => {
-  for (const index in request.body) {
-    request.body[index] = typeof request.body[index] === 'string'
-      ? request.body[index].trim() : request.body[index];
-  }
-  next();
-};
-
-export default trimInputs;
