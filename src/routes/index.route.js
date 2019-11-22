23cd0bbd20ff5d96dfc29209787daecf02d7bd1f
femiofficial <alayesanmifemi@gmail.com>
2019-11-22 15:03:59 +0100

    Added README as index route file

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import path from 'path';
 import authRoutes from './auth.route';
 import userRoutes from './users.routes';
 import questionRoutes from './question.route';
 import answerRoutes from './answers.route';
 
 const indexRouter = express.Router();
+
+indexRouter.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, '../../README.md'));
+});
 
 indexRouter.use('/auth', authRoutes);
 
